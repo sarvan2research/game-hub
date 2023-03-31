ef0d2b57f5f8381b187a6f0222a940ebe487b1ca
saravanakumar r <saravanakumar.ravichandran@oracle.com>
2023-03-31 07:03:10 +0530

    Buidling a navabr

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main "`, lg: `"nav nav" "main aside"` }}
     >
-      <GridItem area={"nav"} bgColor={"coral"}>
-        Nav
+      <GridItem area={"nav"}>
+        <NavBar></NavBar>
       </GridItem>
       <GridItem area={"main"} bgColor={"gold"}>
         Main
