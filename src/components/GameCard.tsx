acb1e15e96211f7e6b7ea6d4f6397bf2fe289578
saravanakumar r <saravanakumar.ravichandran@oracle.com>
2023-04-02 17:09:34 +0530

    Refactor duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,31 +1,34 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import { Box, Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import { icons } from "react-icons";
 import CriticsScore from "./CriticsScore";
 import getCroppedImageUrl from "../service/image-url";
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
-      <Image src={getCroppedImageUrl(game.background_image)}></Image>
-      <CardBody>
-        <Heading fontSize={"2xl"} key={game.id}>
-          {game.name}
-        </Heading>
-        <HStack justifyContent="space-between">
-          <PlatformIconList
-            platforms={game.parent_platforms.map(
-              (platform) => platform.platform
-            )}
-          />
-          <CriticsScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)}></Image>
+        <CardBody>
+          <Heading fontSize={"2xl"} key={game.id}>
+            {game.name}
+          </Heading>
+          <HStack justifyContent="space-between">
+            <PlatformIconList
+              platforms={game.parent_platforms.map(
+                (platform) => platform.platform
+              )}
+            />
+            <CriticsScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
